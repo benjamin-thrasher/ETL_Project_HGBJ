3a8c97d49fb1157cf8dff2693e9173cdf4e1182b
hinaabidi <83368871+hinaabidi@users.noreply.github.com>
2021-09-10 20:11:05 -0400

    Created tables and used pandas to clean the csv and uploaded the data to the talbes

---
+++ Schemas.sql
@@ -0,0 +1,35 @@
+--Create franchise table query 
+
+CREATE TABLE franchise (
+	franchid VARCHAR PRIMARY KEY,
+	franchname VARCHAR,
+	active VARCHAR
+)
+
+
+-- Create teams table query 
+CREATE TABLE teams(
+ 	yearid VARCHAR NOT NULL,
+	teamid VARCHAR NOT NULL,
+	leagueid varchar,
+	franchid varchar,
+	games int,
+	homegames int,
+	wins int,
+	losses int
+)
+
+
+-- Create pitcher table query
+CREATE TABLE pitcher (
+ 	playerid VARCHAR NOT NULL,
+	yearid VARCHAR NOT NULL,
+	teamid VARCHAR,
+	leagueid VARCHAR,
+	wins Int,
+	losses Int,
+	games Int,
+	ipouts Int,
+	primary key (playerid, yearid)
+)
+ 
